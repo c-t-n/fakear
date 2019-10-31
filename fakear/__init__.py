ebae605409e545dc584776465c9983256712477b
radluz <48119760+radluz@users.noreply.github.com>
2019-10-31 20:59:28 +0100

    Update __init__.py
    Added a fix for args list > 9.
    Bash needs curly brackets from the 10th argument forward. Using curly brackets starting from 1 works and allows to use more than 9 args.

--- fakear/__init__.py
+++ fakear/__init__.py
@@ -181,7 +181,7 @@ class Fakear:
                 sub_args = {
                     'length': len(zipped_subs),
                     'arg_line': " && ".join(
-                        ['"${arg}" = "{value}"'.format(arg=arg[0],
+                        ['"${{{arg}}}" = "{value}"'.format(arg=arg[0],
                                                        value=arg[1])
                          for arg in zipped_subs]
                     )
